39670abb9a246ea44ace21134d016dbdca75db5e
Abdulloh31 <abdullah.aflaha.aslam@gmail.com>
2022-09-23 17:19:28 +0700

    upload ke 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,24 +2,60 @@
 // Terima props lalu tampilkan dalam Contact component
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
-import React, { useState } from 'react';
+import React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import ListItemButton from '@mui/material/ListItemButton';
+
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
 
-   
+
     // Contact berisi foto, nama, telepon, dan email
     return (
         <div>
             <div>
-                <ul>
-                    {data.map((value,key) => {
+                <List sx={{ width: '100%',  bgcolor: 'background.paper' }} aria-label="contacts">
+                    {data.map((value, key) => {
                         return (
-                            <li key={key}>{value.name} {value.phone} {value.email} {value.photo}</li>
-                        )
+                            <ListItem disablePadding key={key}>
+                                <ListItemButton>
+                                    <ListItemAvatar >
+                                        <Avatar alt={value.name} src={value.photo} />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={value.name}
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    sx={{ display: 'inline' }}
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="text.primary"
+                                                >
+                                                    {value.phone}<br></br>
+                                                    {value.email}
+                                                </Typography>
+
+                                            </React.Fragment>
+
+                                        }
+                                    />
+                                </ListItemButton>
+                            </ListItem>
+                        );
 
                     })}
+
+                </List>
+                <ul>
+
 
                 </ul>
             </div>
@@ -28,3 +64,5 @@ const Contact = ({ data }) => {
 };
 
 export default Contact;
+
+
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button"; // tambahkan ini untuk import butto
+import { TextField } from '@mui/material';
+import { Grid } from '@mui/material';
 
+import Box from '@mui/material/Box';
 // Form dapat dibuat dengan TextField
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
 
-const ContactForm = ({fnaddContact}) => {
+const ContactForm = ({ fnaddContact }) => {
     // Form berisi name, phone, email, dan photo url
     // Buatlah state newContact berupa objek sesuai dengan data yang ada
     const [name, setName] = useState("");
@@ -22,10 +25,10 @@ const ContactForm = ({fnaddContact}) => {
     const onSubmitHandler = (event) => {
         event.preventDefault();
         const newContact = {
-            name : name,
-            phone : phone,
-            email : email,
-            photo : photo,
+            name: name,
+            phone: phone,
+            email: email,
+            photo: photo,
         };
         fnaddContact(newContact)
         setName("")
@@ -34,34 +37,49 @@ const ContactForm = ({fnaddContact}) => {
         setPhoto("")
     }
 
-   
+
 
     function generate_button(placehol, tipe, valueName, stateName) {
         return (
-            <input
-                placeholder={placehol}
+            // <input
+            //     placeholder={placehol}
+            //     type={tipe}
+            //     onChange={event => onChangeInput(event, stateName)}
+            //     value={valueName}
+            //     style={{ margin: "0.5em" }}
+            // />
+            <TextField
+                size="small"
                 type={tipe}
-                onChange={event => onChangeInput(event, stateName)}
+                label={placehol}
                 value={valueName}
-                style={{ margin: "0.5em" }}
-            />
+                onChange={event => onChangeInput(event, stateName)}
+                variant='filled'
+            ></TextField>
         );
         // return <input placeholder={placehol} type={tipe} />
     }
     return (
         <div>
-            <form style={{ margin: "0.5em 0em" }} onSubmit={onSubmitHandler}>
-                {generate_button('Name', 'text', name, setName)}<br></br>
+
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '90%', margin: "0.5em 0.5em" },
+                }}
+                noValidate
+                autoComplete="off"
+                onSubmit={onSubmitHandler}
+            >
+                {generate_button('Name', 'text', name, setName)}
                 {generate_button('Phone', 'number', phone, setPhone)}<br></br>
                 {generate_button('Email', 'email', email, setEmail)}<br></br>
                 {generate_button('Photo URL', 'text', photo, setPhoto)}<br></br>
                 <Button color="success" type="submit" >Tambah To Do</Button>
-                {/* <input type='text' id="name" name="name">Name</input>
-            <input type='number' id="phone" name="phone">Phone</input>
-            <input type='text' id="email" name="email">Email</input>
-            <input type='text' id="photo" name="photo">Photo</input>
-            <button type="submit">Add New</button> */}
-            </form>
+            </Box>
+
+            {/* <form style={{ margin: "0.5em 0em" }} onSubmit={onSubmitHandler}>
+            </form> */}
         </div>
     );
 };
